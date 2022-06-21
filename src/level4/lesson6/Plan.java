18bbeefb1e9ece918712432af688a37dcdf41911
Roman Pogorelov <rmk140987@gmail.com>
2022-06-21 09:48:37 +0300

    #24 SQL, DB. PK/FK. JOIN types. ACID.

---
+++ src/level4/lesson6/Plan.java
@@ -0,0 +1,12 @@
+package level4.lesson6;
+
+public class Plan {
+//      Базы данных
+//      Установка postgresql & pgAdmin
+//      SQL, CRUD
+//      PK Primary key / FK Foreign key
+
+//      * join types
+//      * relation types
+//      * Транзакция и ACID
+}
