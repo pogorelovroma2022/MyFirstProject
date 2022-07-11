a1ea9c6725c0c968055033d0a99578ec327a38f3
Roman Pogorelov <rmk140987@gmail.com>
2022-07-11 21:47:09 +0300

    № 29 MVC, Spring MVC, Spring Boot, Spring Data. Thymeleaf. Table and Form.

---
+++ src/main/java/level4/lesson11/Plan.java
@@ -0,0 +1,13 @@
+package level4.lesson11;
+
+public class Plan {
+    // MVC, Spring MVC
+    // Spring Boot
+    // Spring Data
+    // Spring MVC: @Controller, @GetMapping, @RequestParam, @PostMapping, @ModelAttribute
+    // Thymeleaf
+    // html: table, form.
+
+    // Web, Java, Database
+    // MVC: Model, View, Controller
+}
